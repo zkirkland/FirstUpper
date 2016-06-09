133865236445bcc988de656b79ae821430403946
zkirkland <zachboy82@gmail.com>
2016-06-09 10:16:05 -0500

    New repo commit. Using new C# .NET template from Kasper Graversen.

--- FirstUpper/Main.cs
+++ FirstUpper/Main.cs
@@ -6,22 +6,21 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 using Kbg.NppPluginNET.PluginInfrastructure;
+using System.Collections.Generic;
 
 namespace Kbg.NppPluginNET
 {
     class Main
     {
+        static string version = "FirstUpper Notepad++ Plugin\nVersion 0.03.00\nCopyright Zach Kirkland\nzkirkland514@gmail.com\nAll Rights Reserved";
         internal const string PluginName = "FirstUpper";
         static string iniFilePath = null;
         static bool someSetting = false;
-        static frmMyDlg frmMyDlg = null;
-        static int idMyDlg = -1;
-        static Bitmap tbBmp = Properties.Resources.star;
-        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
-        static Icon tbIcon = null;
+        static List<string> forbidden = new List<string>();
+        static List<char> ignoreChars = new List<char> { ' ', '\t', '\n', '.', '!', '?' };
 
         public static void OnNotification(ScNotification notification)
-        {  
+        {
             // This method is invoked whenever something is happening in notepad++
             // use eg. as
             // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
@@ -41,18 +40,9 @@ namespace Kbg.NppPluginNET
             iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
             someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
 
-            PluginBase.SetCommand(0, "MyMenuCommand", myMenuFunction, new ShortcutKey(false, false, false, Keys.None));
-            PluginBase.SetCommand(1, "MyDockableDialog", myDockableDialog); idMyDlg = 1;
-        }
-
-        internal static void SetToolBarIcon()
-        {
-            toolbarIcons tbIcons = new toolbarIcons();
-            tbIcons.hToolbarBmp = tbBmp.GetHbitmap();
-            IntPtr pTbIcons = Marshal.AllocHGlobal(Marshal.SizeOf(tbIcons));
-            Marshal.StructureToPtr(tbIcons, pTbIcons, false);
-            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_ADDTOOLBARICON, PluginBase._funcItems.Items[idMyDlg]._cmdID, pTbIcons);
-            Marshal.FreeHGlobal(pTbIcons);
+            PluginBase.SetCommand(0, "Capitalize First Letter", capitalizeFirstLetter, new ShortcutKey(true, true, false, Keys.U));
+            PluginBase.SetCommand(1, "Capitalize First Word of Sentences", capSentence);
+            PluginBase.SetCommand(2, "Plugin Info", firstUpperInfo);
         }
 
         internal static void PluginCleanUp()
@@ -60,47 +50,178 @@ namespace Kbg.NppPluginNET
             Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
         }
 
-
-        internal static void myMenuFunction()
+        #region " Menu functions "
+        internal static void capitalizeFirstLetter()
         {
-            MessageBox.Show("Hello N++!");
-        }
-
-        internal static void myDockableDialog()
-        {
-            if (frmMyDlg == null)
+            // only call this once per notepad++ session
+            if (forbidden.Count == 0)
             {
-                frmMyDlg = new frmMyDlg();
+                // get the list of forbidden words
+                getForbiddenWords();
+            }
 
-                using (Bitmap newBmp = new Bitmap(16, 16))
+            // get current scintilla instance
+            var currentScint = PluginBase.GetCurrentScintilla();
+
+            try
+            {
+                // get size of the selected text
+                int bufSz = (int)Win32.SendMessage(currentScint, SciMsg.SCI_GETSELTEXT, 0, 0);
+
+                // init stringbuilder object to size
+                StringBuilder selectedText = new StringBuilder(bufSz);
+
+                // get selected text from editor
+                Win32.SendMessage(currentScint, SciMsg.SCI_GETSELTEXT, 0, selectedText);
+
+                StringBuilder word = new StringBuilder();
+                char firstLetter = new char();
+                int firstLetterIndex = 0;
+
+                // for the length of the selected text...
+                for (int i = 0; i < selectedText.Length; i++)
                 {
-                    Graphics g = Graphics.FromImage(newBmp);
-                    ColorMap[] colorMap = new ColorMap[1];
-                    colorMap[0] = new ColorMap();
-                    colorMap[0].OldColor = Color.Fuchsia;
-                    colorMap[0].NewColor = Color.FromKnownColor(KnownColor.ButtonFace);
-                    ImageAttributes attr = new ImageAttributes();
-                    attr.SetRemapTable(colorMap);
-                    g.DrawImage(tbBmp_tbTab, new Rectangle(0, 0, 16, 16), 0, 0, 16, 16, GraphicsUnit.Pixel, attr);
-                    tbIcon = Icon.FromHandle(newBmp.GetHicon());
+                    // if the current character is not a whitespace character...
+                    if (ignoreChars.Contains(selectedText[i]) == false)
+                    {
+                        if (i > 0)
+                        {
+                            // if the previous char is a whitespace char then
+                            // the current char is the first letter of the word
+                            if (ignoreChars.Contains(selectedText[i - 1]) == true)
+                            {
+                                // set the index to the index of the first letter of this word
+                                firstLetterIndex = i;
+                            }
+                        }
+
+                        // build the string one char at a time.
+                        word.Append(selectedText[i]);
+                    }
+
+                    // out of bounds check
+                    if ((i + 1) < selectedText.Length - 1)
+                    {
+                        // if the next char is whitespace char that means the current
+                        // char is the last char of the current word.
+                        if (ignoreChars.Contains(selectedText[i + 1]) == true)
+                        {
+                            // make sure it is not a forbidden word
+                            if (forbidden.Contains(word.ToString()) == false && word.Length != 0)
+                            {
+                                // convert the first letter of the word to uppercase
+                                firstLetter = Convert.ToChar(word[0].ToString().ToUpper());
+                                // replace the correct char in the selected text with its uppercase letter
+                                selectedText.Replace(selectedText[firstLetterIndex], firstLetter, firstLetterIndex, 1);
+                                // clear the word to make ready for the next one
+                                word.Clear();
+                            }
+                            // otherwise we do not want the word to be capitalized...
+                            else
+                            {
+                                // clear the word to make ready for the next one
+                                word.Clear();
+                            }
+                        }
+                    }
+                    // otherwise this is the last word of the selected text
+                    else if (i == selectedText.Length - 1)
+                    {
+                        // make sure it is not a forbidden word
+                        if (forbidden.Contains(word.ToString()) == false && word.Length != 0)
+                        {
+                            // convert the first letter of the word to uppercase
+                            firstLetter = Convert.ToChar(word[0].ToString().ToUpper());
+                            // replace the correct char in the selected text with its uppercase letter
+                            selectedText.Replace(selectedText[firstLetterIndex], firstLetter, firstLetterIndex, 1);
+                            break;
+                        }
+                    }
                 }
-
-                NppTbData _nppTbData = new NppTbData();
-                _nppTbData.hClient = frmMyDlg.Handle;
-                _nppTbData.pszName = "My dockable dialog";
-                _nppTbData.dlgID = idMyDlg;
-                _nppTbData.uMask = NppTbMsg.DWS_DF_CONT_RIGHT | NppTbMsg.DWS_ICONTAB | NppTbMsg.DWS_ICONBAR;
-                _nppTbData.hIconTab = (uint)tbIcon.Handle;
-                _nppTbData.pszModuleName = PluginName;
-                IntPtr _ptrNppTbData = Marshal.AllocHGlobal(Marshal.SizeOf(_nppTbData));
-                Marshal.StructureToPtr(_nppTbData, _ptrNppTbData, false);
-
-                Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_DMMREGASDCKDLG, 0, _ptrNppTbData);
+                // replace the selected text with the new string
+                Win32.SendMessage(currentScint, SciMsg.SCI_REPLACESEL, 0, selectedText);
             }
-            else
+            catch (Exception e)
             {
-                Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_DMMSHOW, 0, frmMyDlg.Handle);
+                MessageBox.Show(e.ToString());
             }
         }
+
+        // load the list of forbidden words
+        internal static void getForbiddenWords()
+        {
+            using (StreamReader file = new StreamReader("plugins\\FirstUpperForbiddenWords.txt"))
+            {
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line.ToLower();
+                    line.Trim(' ');
+                    // only load words without "*" in front of them.
+                    if (line[0] != '*')
+                    {
+                        forbidden.Add(line);
+                    }
+                } // end while readline
+            }
+        }
+
+        internal static void firstUpperInfo()
+        {
+            MessageBox.Show(version);
+        }
+
+        internal static void capSentence()
+        {
+            // get current scintilla instance
+            var currentScint = PluginBase.GetCurrentScintilla();
+
+            try
+            {
+                // get size of the document
+                int bufSz = (int)Win32.SendMessage(currentScint, SciMsg.SCI_GETLENGTH, 0, 0);
+                // init stringbuilder object to size
+                StringBuilder allText = new StringBuilder(bufSz + 1);
+
+                // get all text from editor
+                Win32.SendMessage(currentScint, SciMsg.SCI_GETTEXT, bufSz + 1, allText);
+
+                char firstLetter = new char();
+                bool capAfterPunct = true;
+
+                // for the length of the selected text...
+                for (int i = 0; i < allText.Length; i++)
+                {
+                    // if there is punctuation that ends a sentence
+                    // the next word should be capitalized
+                    if (allText[i] == '.' || allText[i] == '?' || allText[i] == '!')
+                    {
+                        capAfterPunct = true;
+                    }
+
+                    if (capAfterPunct == true)
+                    {
+                        // if the current character is not a whitespace character...
+                        if (ignoreChars.Contains(allText[i]) == false)
+                        {
+                            // convert the first letter of the word to uppercase
+                            firstLetter = Convert.ToChar(allText[i].ToString().ToUpper());
+                            // replace the correct char in the selected text with its uppercase letter
+                            allText.Replace(allText[i], firstLetter, i, 1);
+                            // revert to false until beginning of next sentence
+                            capAfterPunct = false;
+                        }
+                    }
+                }
+                // replace the document text with the new text
+                Win32.SendMessage(currentScint, SciMsg.SCI_SETTEXT, 0, allText);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        #endregion
     }
 }
---
+++ changelog.md
@@ -0,0 +1,13 @@
+#####Version 0.02 -- 06.06.2016
+Improvements to how the code handles the text.
+
+* Now leaves the original string intact and finds the first letter of each word and capitalizes the letter without splitting the string into individial words.
+* Added menu option to show plugin info such as version number and etc.
+* Fix bug where selected words were sometimes not being capitalized when they were supposed to be.
+* Fix bug where plugin would crash if more than two spaces were selected in between words.
+* Add menu option and capability to capitalize the first letter of the first word of all of the sentences in the document.
+
+#####Version 0.01 -- 06.03.2016
+Initial commit of code and small bug fixes.
+
+* Splits text based on whitespace and capitalizes the resulting words if the word is not in the "forbidden" list.
