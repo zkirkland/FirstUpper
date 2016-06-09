5698f4aecc0f0c8baac703af71cc2b99d43de1c5
zkirkland <zachboy82@gmail.com>
2016-06-09 13:40:50 -0500

    Update the rest of the plugin methods with the new .NET methods. (scintillaGateway, etc.)

--- FirstUpper/Main.cs
+++ FirstUpper/Main.cs
@@ -21,7 +21,7 @@ namespace Kbg.NppPluginNET
 
         public static void OnNotification(ScNotification notification)
         {
-            // This method is invoked whenever something is happening in notepad++
+            // This method is invoked whenever something is happening in notepad-plus-plus
             // use eg. as
             // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
             // { ... }
@@ -40,9 +40,9 @@ namespace Kbg.NppPluginNET
             iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
             someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
 
-            PluginBase.SetCommand(0, "Capitalize First Letter", capitalizeFirstLetter, new ShortcutKey(true, true, false, Keys.U));
-            PluginBase.SetCommand(1, "Capitalize First Word of Sentences", capSentence);
-            PluginBase.SetCommand(2, "Plugin Info", firstUpperInfo);
+            PluginBase.SetCommand(0, "Capitalize First Letter", CapitalizeFirstLetter, new ShortcutKey(true, true, false, Keys.U));
+            PluginBase.SetCommand(1, "Capitalize First Word of Sentences", CapSentence);
+            PluginBase.SetCommand(2, "Plugin Info", FirstUpperInfo);
         }
 
         internal static void PluginCleanUp()
@@ -51,95 +51,93 @@ namespace Kbg.NppPluginNET
         }
 
         #region " Menu functions "
-        internal static void capitalizeFirstLetter()
+        internal static void CapitalizeFirstLetter()
         {
-            // only call this once per notepad++ session
+            // Only call this once per notepad-plus-plus session.
             if (forbidden.Count == 0)
             {
-                // get the list of forbidden words
-                getForbiddenWords();
+                // Get the list of forbidden words.
+                GetForbiddenWords();
             }
 
-            // get current scintilla instance
-            var currentScint = PluginBase.GetCurrentScintilla();
+            // Get current scintilla instance.
+            IntPtr currentScint = PluginBase.GetCurrentScintilla();
+            ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
 
             try
             {
-                // get size of the selected text
-                int bufSz = (int)Win32.SendMessage(currentScint, SciMsg.SCI_GETSELTEXT, 0, 0);
+                // Get scintilla gateway.
+                string selectedText = scintillaGateway.GetSelText();
+                MessageBox.Show("selText created: " + selectedText);
 
-                // init stringbuilder object to size
-                StringBuilder selectedText = new StringBuilder(bufSz);
-
-                // get selected text from editor
-                Win32.SendMessage(currentScint, SciMsg.SCI_GETSELTEXT, 0, selectedText);
+                // Change to char array for easy editing of single chars.
+                char[] charArraySelText = selectedText.ToCharArray();
 
                 StringBuilder word = new StringBuilder();
                 char firstLetter = new char();
                 int firstLetterIndex = 0;
 
-                // for the length of the selected text...
+                // For the length of the selected text...
                 for (int i = 0; i < selectedText.Length; i++)
                 {
-                    // if the current character is not a whitespace character...
+                    // If the current character is not a whitespace character...
                     if (ignoreChars.Contains(selectedText[i]) == false)
                     {
-                        if (i > 0)
+                        // If the previous char is a whitespace char then
+                        // the current char is the first letter of the word.
+                        if (i > 0 && ignoreChars.Contains(selectedText[i - 1]))
                         {
-                            // if the previous char is a whitespace char then
-                            // the current char is the first letter of the word
-                            if (ignoreChars.Contains(selectedText[i - 1]) == true)
-                            {
-                                // set the index to the index of the first letter of this word
-                                firstLetterIndex = i;
-                            }
+                            // Set the index to the index of the first letter of this word.
+                            firstLetterIndex = i;
                         }
 
-                        // build the string one char at a time.
+                        // Build the string one char at a time.
                         word.Append(selectedText[i]);
                     }
 
-                    // out of bounds check
+                    // Out of bounds check.
                     if ((i + 1) < selectedText.Length - 1)
                     {
-                        // if the next char is whitespace char that means the current
+                        // If the next char is whitespace char that means the current
                         // char is the last char of the current word.
                         if (ignoreChars.Contains(selectedText[i + 1]) == true)
                         {
-                            // make sure it is not a forbidden word
+                            // Make sure it is not a forbidden word.
                             if (forbidden.Contains(word.ToString()) == false && word.Length != 0)
                             {
-                                // convert the first letter of the word to uppercase
-                                firstLetter = Convert.ToChar(word[0].ToString().ToUpper());
-                                // replace the correct char in the selected text with its uppercase letter
-                                selectedText.Replace(selectedText[firstLetterIndex], firstLetter, firstLetterIndex, 1);
-                                // clear the word to make ready for the next one
+                                // Convert the first letter of the word to uppercase.
+                                firstLetter = Convert.ToChar(word[0].ToString().ToUpperInvariant());
+                                // Replace the correct char in the selected text with its uppercase letter.
+                                charArraySelText.SetValue(firstLetter, firstLetterIndex);
+                                // Clear the word to make ready for the next one.
                                 word.Clear();
                             }
-                            // otherwise we do not want the word to be capitalized...
+                            // Otherwise we do not want the word to be capitalized...
                             else
                             {
-                                // clear the word to make ready for the next one
+                                // Clear the word to make ready for the next one.
                                 word.Clear();
                             }
                         }
                     }
-                    // otherwise this is the last word of the selected text
+                    // Otherwise this is the last word of the selected text.
                     else if (i == selectedText.Length - 1)
                     {
-                        // make sure it is not a forbidden word
-                        if (forbidden.Contains(word.ToString()) == false && word.Length != 0)
+                        // Make sure it is not a forbidden word.
+                        if (!forbidden.Contains(word.ToString()) && word.Length != 0)
                         {
-                            // convert the first letter of the word to uppercase
-                            firstLetter = Convert.ToChar(word[0].ToString().ToUpper());
-                            // replace the correct char in the selected text with its uppercase letter
-                            selectedText.Replace(selectedText[firstLetterIndex], firstLetter, firstLetterIndex, 1);
+                            // Convert the first letter of the word to uppercase.
+                            firstLetter = Convert.ToChar(word[0].ToString().ToUpperInvariant());
+                            // Replace the correct char in the selected text with its uppercase letter.
+                            charArraySelText.SetValue(firstLetter, firstLetterIndex);
                             break;
                         }
                     }
                 }
-                // replace the selected text with the new string
-                Win32.SendMessage(currentScint, SciMsg.SCI_REPLACESEL, 0, selectedText);
+                // Convert the char array back to string.
+                selectedText = new string(charArraySelText);
+                // Replace the selected text with the new string
+                scintillaGateway.ReplaceSel(selectedText);
             }
             catch (Exception e)
             {
@@ -148,7 +146,7 @@ namespace Kbg.NppPluginNET
         }
 
         // load the list of forbidden words
-        internal static void getForbiddenWords()
+        internal static void GetForbiddenWords()
         {
             using (StreamReader file = new StreamReader("plugins\\FirstUpperForbiddenWords.txt"))
             {
@@ -156,8 +154,8 @@ namespace Kbg.NppPluginNET
 
                 while ((line = file.ReadLine()) != null)
                 {
-                    line = line.ToLower();
-                    line.Trim(' ');
+                    line = line.ToLowerInvariant();
+                    line = line.Trim(' ');
                     // only load words without "*" in front of them.
                     if (line[0] != '*')
                     {
@@ -167,55 +165,56 @@ namespace Kbg.NppPluginNET
             }
         }
 
-        internal static void firstUpperInfo()
+        internal static void FirstUpperInfo()
         {
             MessageBox.Show(version);
         }
 
-        internal static void capSentence()
+        internal static void CapSentence()
         {
-            // get current scintilla instance
-            var currentScint = PluginBase.GetCurrentScintilla();
+            // Get current scintilla instance.
+            IntPtr currentScint = PluginBase.GetCurrentScintilla();
+            ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
 
             try
             {
-                // get size of the document
-                int bufSz = (int)Win32.SendMessage(currentScint, SciMsg.SCI_GETLENGTH, 0, 0);
-                // init stringbuilder object to size
-                StringBuilder allText = new StringBuilder(bufSz + 1);
+                // Get the length of the document.
+                int length = scintillaGateway.GetLength();
+                // Get the text in the document.
+                string allText = scintillaGateway.GetText(length);
 
-                // get all text from editor
-                Win32.SendMessage(currentScint, SciMsg.SCI_GETTEXT, bufSz + 1, allText);
+                // Convert the text to char array for easy manipulation.
+                char[] charArrayAllText = allText.ToCharArray();
 
                 char firstLetter = new char();
                 bool capAfterPunct = true;
 
-                // for the length of the selected text...
+                // For the length of the selected text...
                 for (int i = 0; i < allText.Length; i++)
                 {
-                    // if there is punctuation that ends a sentence
-                    // the next word should be capitalized
+                    // If there is punctuation that ends a sentence
+                    // the next word should be capitalized.
                     if (allText[i] == '.' || allText[i] == '?' || allText[i] == '!')
                     {
                         capAfterPunct = true;
                     }
 
-                    if (capAfterPunct == true)
+                    if (capAfterPunct && !ignoreChars.Contains(allText[i]))
                     {
-                        // if the current character is not a whitespace character...
-                        if (ignoreChars.Contains(allText[i]) == false)
-                        {
-                            // convert the first letter of the word to uppercase
-                            firstLetter = Convert.ToChar(allText[i].ToString().ToUpper());
-                            // replace the correct char in the selected text with its uppercase letter
-                            allText.Replace(allText[i], firstLetter, i, 1);
-                            // revert to false until beginning of next sentence
-                            capAfterPunct = false;
-                        }
+                        // If the current character is not a whitespace character
+                        // convert the first letter of the word to uppercase.
+                        firstLetter = Convert.ToChar(allText[i].ToString().ToUpperInvariant());
+                        // Replace the correct char in the selected text with its uppercase letter.
+                        charArrayAllText.SetValue(firstLetter, i);
+                        // Revert to false until beginning of next sentence.
+                        capAfterPunct = false;
                     }
                 }
-                // replace the document text with the new text
-                Win32.SendMessage(currentScint, SciMsg.SCI_SETTEXT, 0, allText);
+                // Convert char array back to string.
+                allText = new string(charArrayAllText);
+                // Replace the document text with the new text.
+                scintillaGateway.SelectAll();
+                scintillaGateway.ReplaceSel(allText);
             }
             catch (Exception e)
             {
