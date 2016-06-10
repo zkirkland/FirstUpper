00191d6681c11a3251ae272a0edaa37ac8cd338b
zkirkland <zachboy82@gmail.com>
2016-06-10 09:14:35 -0500

    -Add capability to find and capitalize all Markdown titles in a document. -Make menu options more clear -Correct erroneous capitalization in titles when selecting "Capitalize Selected Title" or "Capitalize All Markdown Titles" by first converting to lowercase then capitalizing correct words that are not in the "forbidden" list.

--- FirstUpper/Main.cs
+++ FirstUpper/Main.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 using Kbg.NppPluginNET.PluginInfrastructure;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Kbg.NppPluginNET
 {
@@ -17,7 +18,7 @@ namespace Kbg.NppPluginNET
         static string iniFilePath = null;
         static bool someSetting = false;
         static List<string> forbidden = new List<string>();
-        static List<char> ignoreChars = new List<char> { ' ', '\t', '\n', '.', '!', '?' };
+        static List<char> ignoreChars = new List<char> { ' ', '\t', '\n', '.', '!', '?', '#', '\r' };
 
         public static void OnNotification(ScNotification notification)
         {
@@ -40,9 +41,10 @@ namespace Kbg.NppPluginNET
             iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
             someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
 
-            PluginBase.SetCommand(0, "Capitalize First Letter", CapitalizeFirstLetter, new ShortcutKey(true, true, false, Keys.U));
-            PluginBase.SetCommand(1, "Capitalize First Word of Sentences", CapSentence);
-            PluginBase.SetCommand(2, "Plugin Info", FirstUpperInfo);
+            PluginBase.SetCommand(0, "Capitalize Selected Title", CapitalizeTitle, new ShortcutKey(true, true, false, Keys.U));
+            PluginBase.SetCommand(1, "Capitalize All Markdown Titles", CapitalizeMDTitles);
+            PluginBase.SetCommand(2, "Capitalize First Word of All Sentences", CapSentence);
+            PluginBase.SetCommand(3, "Plugin Info", FirstUpperInfo);
         }
 
         internal static void PluginCleanUp()
@@ -51,7 +53,10 @@ namespace Kbg.NppPluginNET
         }
 
         #region " Menu functions "
-        internal static void CapitalizeFirstLetter()
+        /// <summary>
+        /// Capitalize user selected text based on Chicago Manual of Style.
+        /// </summary>
+        internal static void CapitalizeTitle()
         {
             // Only call this once per notepad-plus-plus session.
             if (forbidden.Count == 0)
@@ -60,34 +65,35 @@ namespace Kbg.NppPluginNET
                 GetForbiddenWords();
             }
 
-            // Get current scintilla instance.
             IntPtr currentScint = PluginBase.GetCurrentScintilla();
             ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
 
             try
             {
-                // Get scintilla gateway.
+                // If selected text is not passed in, make sure we get it here.
                 string selectedText = scintillaGateway.GetSelText();
-                MessageBox.Show("selText created: " + selectedText);
-
+                // Convert to lower case in case some words are already
+                // capitalized that should not be capitalized.
+                selectedText = selectedText.ToLowerInvariant();
                 // Change to char array for easy editing of single chars.
                 char[] charArraySelText = selectedText.ToCharArray();
 
                 StringBuilder word = new StringBuilder();
                 char firstLetter = new char();
-                int firstLetterIndex = 0;
+                int firstLetterIndex = -1;
+                bool firstWord = true;
 
                 // For the length of the selected text...
                 for (int i = 0; i < selectedText.Length; i++)
                 {
                     // If the current character is not a whitespace character...
-                    if (ignoreChars.Contains(selectedText[i]) == false)
+                    if (!ignoreChars.Contains(selectedText[i]))
                     {
-                        // If the previous char is a whitespace char then
-                        // the current char is the first letter of the word.
-                        if (i > 0 && ignoreChars.Contains(selectedText[i - 1]))
+                        // If first letter index is negative that means this
+                        // is the first letter of the word. Set the index accordingly.
+                        if (i >= 0 && firstLetterIndex < 0)
                         {
-                            // Set the index to the index of the first letter of this word.
+                            // Set the first letter index to the index of the first letter of this word.
                             firstLetterIndex = i;
                         }
 
@@ -100,10 +106,10 @@ namespace Kbg.NppPluginNET
                     {
                         // If the next char is whitespace char that means the current
                         // char is the last char of the current word.
-                        if (ignoreChars.Contains(selectedText[i + 1]) == true)
+                        if (ignoreChars.Contains(selectedText[i + 1]))
                         {
                             // Make sure it is not a forbidden word.
-                            if (forbidden.Contains(word.ToString()) == false && word.Length != 0)
+                            if ((firstWord || !forbidden.Contains(word.ToString())) && word.Length != 0)
                             {
                                 // Convert the first letter of the word to uppercase.
                                 firstLetter = Convert.ToChar(word[0].ToString().ToUpperInvariant());
@@ -111,27 +117,30 @@ namespace Kbg.NppPluginNET
                                 charArraySelText.SetValue(firstLetter, firstLetterIndex);
                                 // Clear the word to make ready for the next one.
                                 word.Clear();
+                                // Reset first letter and first word indicators.
+                                firstLetterIndex = -1;
+                                firstWord = false;
                             }
                             // Otherwise we do not want the word to be capitalized...
                             else
                             {
                                 // Clear the word to make ready for the next one.
                                 word.Clear();
+                                // Reset first letter indicator.
+                                firstLetterIndex = -1;
                             }
                         }
                     }
                     // Otherwise this is the last word of the selected text.
                     else if (i == selectedText.Length - 1)
                     {
-                        // Make sure it is not a forbidden word.
-                        if (!forbidden.Contains(word.ToString()) && word.Length != 0)
-                        {
-                            // Convert the first letter of the word to uppercase.
-                            firstLetter = Convert.ToChar(word[0].ToString().ToUpperInvariant());
-                            // Replace the correct char in the selected text with its uppercase letter.
-                            charArraySelText.SetValue(firstLetter, firstLetterIndex);
-                            break;
-                        }
+                        // Convert the first letter of the word to uppercase.
+                        firstLetter = Convert.ToChar(word[0].ToString().ToUpperInvariant());
+                        // Replace the correct char in the selected text with its uppercase letter.
+                        charArraySelText.SetValue(firstLetter, firstLetterIndex);
+                        // Reset first letter indicator.
+                        firstLetterIndex = -1;
+                        break;
                     }
                 }
                 // Convert the char array back to string.
@@ -145,7 +154,9 @@ namespace Kbg.NppPluginNET
             }
         }
 
-        // load the list of forbidden words
+        /// <summary>
+        /// Get the list of "forbidden" words from the text file.
+        /// </summary>
         internal static void GetForbiddenWords()
         {
             using (StreamReader file = new StreamReader("plugins\\FirstUpperForbiddenWords.txt"))
@@ -165,11 +176,18 @@ namespace Kbg.NppPluginNET
             }
         }
 
+        /// <summary>
+        /// Display a message box with plugin information in it.
+        /// </summary>
         internal static void FirstUpperInfo()
         {
             MessageBox.Show(version);
         }
 
+        /// <summary>
+        /// Find and capitalize the first letter of the beginning of all
+        /// sentences in a document.
+        /// </summary>
         internal static void CapSentence()
         {
             // Get current scintilla instance.
@@ -194,9 +212,14 @@ namespace Kbg.NppPluginNET
                 {
                     // If there is punctuation that ends a sentence
                     // the next word should be capitalized.
-                    if (allText[i] == '.' || allText[i] == '?' || allText[i] == '!')
+                    if (allText[i] == '.' || allText[i] == '?' || allText[i] == '!' || allText[i] == '\r')
                     {
                         capAfterPunct = true;
+                    }
+                    // Don't capitalize Markdown titles in this method.
+                    else if (allText[i] == '#')
+                    {
+                        capAfterPunct = false;
                     }
 
                     if (capAfterPunct && !ignoreChars.Contains(allText[i]))
@@ -220,6 +243,67 @@ namespace Kbg.NppPluginNET
             {
                 MessageBox.Show(e.ToString());
             }
+        }
+
+        /// <summary>
+        /// Find and capitalize all Markdown Titles in the document.
+        /// </summary>
+        internal static void CapitalizeMDTitles()
+        {
+            // Get scintilla gateway.
+            IntPtr currentScint = PluginBase.GetCurrentScintilla();
+            ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
+
+            string line;
+
+            // Get the number of lines in the document.
+            int numLines = scintillaGateway.GetLineCount();
+
+            // Traverse through each line.
+            for (int i = 0; i < numLines; i++)
+            {
+                // Set line to the current line.
+                line = scintillaGateway.GetLine(i);
+
+                // Check each character in the line to see if it begins
+                // with a '#'.
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == '#')
+                    {
+                        // If it begins with '#', select the line and call
+                        // the CapitalizeTitle method with these parameters.
+                        scintillaGateway.GotoLine(i);
+                        scintillaGateway.SetSel(scintillaGateway.PositionFromLine(i), scintillaGateway.GetLineEndPosition(i));
+                        CapitalizeTitle();
+                        break;
+                    }
+                    // Support for Setext headers.
+                    else if ((line[j] == '-' || line[j] == '=') && !IsAlphaNumeric(line))
+                    {
+                        // If it begins with '-' or '=', select the previous line and call
+                        // the CapitalizeTitle method with these parameters.
+                        scintillaGateway.GotoLine(i - 1);
+                        scintillaGateway.SetSel(scintillaGateway.PositionFromLine(i - 1), scintillaGateway.GetLineEndPosition(i - 1));
+                        CapitalizeTitle();
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check to see if the string has letters or numbers in it.
+        /// </summary>
+        /// <param name="strToCheck"></param>
+        /// <returns></returns>
+        internal static bool IsAlphaNumeric(string strToCheck)
+        {
+            // If the line that contains '=' or '-' has letters or numbers
+            // on it, then the previous line is not a title and therefore
+            // should not be capitalized.
+            Regex rg = new Regex(@"[a-zA-Z0-9]+");
+            return rg.IsMatch(strToCheck);
         }
         #endregion
     }
